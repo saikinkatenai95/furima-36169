8c942ab11067e8df8cf0b3f746898f29f06d2894
saikinkatenai95 <saikinkatenai95@outlook.jp>
2021-08-26 21:14:45 +0900

    新規登録のバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :lastname, presence: true
-  validates :firstname, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+  validates :lastname
+  validates :firstname
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/} do
   validates :lastname_japanese, presence: true
   validates :firstname_japanese, presence: true
+  end
   validates :birthday, presence: true  
-
+  validates :password, length: { minimum: 6},format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
   has_many :items
 end
