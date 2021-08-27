618f228647a48b393cebd035d588322da8f76d49
saikinkatenai95 <saikinkatenai95@outlook.jp>
2021-08-27 21:40:09 +0900

    userモデルのテストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,15 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    password              {'000000'}
+    gimei = Gimei.name
+
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    lastname              {gimei.last.kanji }
+    firstname             {gimei.first.kanji}
+    lastname_japanese     {gimei.last.katakana}
+    firstname_japanese    {gimei.first.katakana}
+    birthday              {Faker::Date.between(from: '2014-09-23', to: '2018-09-25')}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,91 @@
 require 'rails_helper'
-
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+    describe "ユーザー新規登録" do
+      context "新規登録できない時" do
+        it "nicknameが空では登録できない" do
+        @user.nickname = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+        end
+
+        it "emailが空では登録できない" do
+          @user.email = ""
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Email can't be blank")
+        end
+
+        it "passwordが空では登録できない" do
+          @user.password = ""
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Password can't be blank")
+        
+        end
+
+        
+        it "lastnameが空では登録できない" do
+          
+          
+        end
+        
+        it "firstnameが空では登録できない" do
+          
+          
+        end
+        
+        it "lastname_japanaseが空では登録できない" do
+          
+          
+        end
+        
+        it "firstname_japanaseが空では登録できない" do
+          
+          
+        end
+        
+        it "dateが空では登録できない" do
+          
+          
+        end
+        
+        it "lastnameが漢字以外では登録できない" do
+          
+          
+        end
+        
+        it "firstnameが漢字以外では登録できない" do
+          
+          
+        end
+        
+        it "lastname_japaneseがカタカナ以外では登録できない" do
+          
+          
+        end
+        
+        it "firstname_japaneseがカタカナ以外では登録できない" do
+          
+          
+        end
+
+        it "passwordとpassword_confirmationが不一致では登録できない" do
+        
+        
+        end
+
+        it "passwordが5文字以下では登録できない" do
+        
+        
+        end
+        
+        it "重複したemailが存在する場合は登録できない" do
+        
+        
+        end
+
+    end
+  end
 end
