46f2d0f9c0e81fe31b1ed6da12b00a1218f8d2d9
saikinkatenai95 <saikinkatenai95@outlook.jp>
2021-08-29 13:20:18 +0900

    actvie strage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,11 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
-    
+
   end
 
+
+private
+def item_params
+  params.require(:item).permit(:name, :image, :info, :category_id, :item_status, :delivery_fee_id, :prefecture_id, :read_time_id, :price).merge(user_id: current_user.id)
   end
