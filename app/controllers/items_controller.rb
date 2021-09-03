fd5136e9077b3878afd6636f366fa98d4002288b
saikinkatenai95 <saikinkatenai95@outlook.jp>
2021-09-03 13:37:46 +0900

    リダイレクト処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+  before_action :move_to_index, except: [:index]
 
   def index
     @items = Item.all
@@ -22,5 +24,11 @@ private
 
   def item_params
     params.require(:item).permit(:name, :image, :info, :category_id, :itemcondition_id, :deliveryfee_id, :prefecture_id, :readtime_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless  user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
