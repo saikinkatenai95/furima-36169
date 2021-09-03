28c8f98c3512e28daf8e1e05397950507b91a850
saikinkatenai95 <saikinkatenai95@outlook.jp>
2021-09-03 15:18:11 +0900

    Revert "itemモデルテストコード修正"
    This reverts commit 72de1be34d6f4ade63560c260e1f68f59c4ed36e.

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :name, presence: true
   validates :info, presence: true
-  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: {with: /\A[0-9]+\z/}
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -77,18 +77,6 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
-
-      it "priceに半角数字以外が含まれている場合出品できない" do
-        @item.price = "１０００en"
-        @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not a number")
-      end
-
-      it "userが紐づいていなければ出品できない" do
-        @item.user = nil
-        @item.valid?
-        expect(@item.errors.full_messages).to include("User must exist")
-      end
     
     end
   end
