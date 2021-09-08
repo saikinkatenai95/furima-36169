5695bf21f3e9fe7ebdb37da0f389916b5b3cb59f
saikinkatenai95 <saikinkatenai95@outlook.jp>
2021-09-08 19:55:54 +0900

    ビュー画面編集

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,12 +1,26 @@
 class BuysController < ApplicationController
 
-def index
-end
+  def index
+    @item = Item.find(params[:item_id])
+  end
 
-def new
-end
+  def new
+  end
 
-def create
-end
+  def create
+    @buy = Buy.create(buy_params)
+    Residence.create(residence_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def buy_params
+    params.merge(user_id: current_user.id, item_id: @item.id)
+  end
+
+  def residence_params
+    params.permit(:postal_code, :prefecture_id, :city, :address, :build_name, :phone_number).merge(buy_id: @buy.id)
+  end
 
 end
