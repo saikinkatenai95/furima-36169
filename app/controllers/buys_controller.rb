9268b6da2e4de9b17419aa773cac80bc4a634543
saikinkatenai95 <saikinkatenai95@outlook.jp>
2021-09-09 18:15:14 +0900

    DB保存できるよう実装

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -2,6 +2,7 @@ class BuysController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @buy_residence = BuyResidence.new
   end
 
   def new
@@ -9,10 +10,10 @@ class BuysController < ApplicationController
   end
 
   def create
-    binding.pry
     @item = Item.find(params[:item_id])
     @buy_residence = BuyResidence.new(buy_params)
     if @buy_residence.valid?
+      pay_item
       @buy_residence.save
     redirect_to root_path
     else
@@ -23,7 +24,16 @@ class BuysController < ApplicationController
   private
 
   def buy_params
-    params.permit(:postal_code, :prefecture_id, :city, :address, :build_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:buy_residence).permit(:postal_code, :prefecture_id, :city, :address, :build_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: buy_params[:token],
+        currency: "jpy"
+      )
   end
 
 end
--- app/models/buy_residence.rb
+++ app/models/buy_residence.rb
@@ -4,11 +4,11 @@ class BuyResidence
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :city, :address, :user_id, :item_id
+    validates :city, :address, :user_id, :item_id, :token
     validates :phone_number, format: {with: /\A\d{10}\z|\A\d{11}\z/, message: "is invalid. Include hyphen(-)"}
   end
   
-    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
     def save
       buy = Buy.create(user_id: user_id, item_id: item_id)
